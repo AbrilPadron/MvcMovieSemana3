bfbcad4a617426fcb91956fbe7f960ad8687a838
Mati <matiaschamarez@gmail.com>
2024-04-11 14:30:15 -0300

    Agregar archivos de proyecto.

---
+++ MvcMovieSemana3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcMovieSemana3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
